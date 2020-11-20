e53b2908a9aae8098fe9cbafebce1133ea4e386e
sida <sliu1@uvm.edu>
2020-11-20 17:57:03 -0500

    eval ids

--- cleaned_version/script-templates/submit_multi.sh
+++ cleaned_version/script-templates/submit_multi.sh
@@ -9,6 +9,6 @@ cd $cwd
 
 source activate thesis-bodies
 
-_CMD_="python _train.py --exp-name=$1 --exp-idx=$2 --body-ids=$3 --seed=$4 $5 --dataset=$dataset --n-timesteps=$6"
+_CMD_="python _train.py --exp-name=$1 --exp-idx=$2 --body-ids=$3 --eval-ids=$4 --seed=$5 $6 --dataset=$dataset --n-timesteps=$7"
 echo $_CMD_
 eval $_CMD_
