3660055772821762f3e750b37bdb94658cf14e26
sida <sliu1@uvm.edu>
2021-01-03 23:51:07 -0500

    update

--- project/experiments/exp_006_body_dataset_2/src/start_experiment.sh
+++ project/experiments/exp_006_body_dataset_2/src/start_experiment.sh
@@ -6,12 +6,12 @@ set -x
 python 0.init.py
 EXP_FOLDER=$(cat .exp_folder)
 
-for seed in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+for seed in 0 1
 do
-    body=300
-    #control
-    sbatch -J $EXP_FOLDER submit.sh python 1.train.py --train_bodies=$body --test_bodies=$body --seed=$seed --train_steps=1e6 --num_venv=1
-    #treatment
-    sbatch -J $EXP_FOLDER submit.sh python 1.train.py --train_bodies=$body --test_bodies=$body --seed=$seed --train_steps=1e6 --num_venv=1 --stack_frames=4
+    for body in 300 400 500 600 301 401 501 601
+    do
+        #treatment
+        sbatch -J $EXP_FOLDER submit.sh python 1.train.py --train_bodies=$body --test_bodies=$body --seed=$seed --train_steps=1e6
+    done
 done
 
