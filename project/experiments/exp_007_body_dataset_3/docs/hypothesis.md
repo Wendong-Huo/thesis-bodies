3e729e46b5befa0e54075550fd8e7eac43909623
sida <sliu1@uvm.edu>
2021-01-05 12:31:55 -0500

    update

--- project/experiments/exp_007_body_dataset_3/docs/hypothesis.md
+++ project/experiments/exp_007_body_dataset_3/docs/hypothesis.md
@@ -1,14 +1,16 @@
 # What is tested?
 
 Can we create a body dataset,
-containing 25 Walker2D (300s) variations, 25 HalfCheetah (400s) variations, 25 Ant (500s) variations, 25 Hopper (600s) variations,
-100 bodies in total.
+containing 20 Walker2D (300s) variations, 20 HalfCheetah (400s) variations, 20 Ant (500s) variations, 20 Hopper (600s) variations,
+80 bodies in total.
 
-All the bodies can be quickly trained using PPO.
+All the bodies can be quickly trained using PPO (with stack_frames=4).
 
 # What are Treatment and Control groups?
 
 # What are the results? (in short)
+80 bodies in output_data/selected_bodies
 
 # Other Thoughts?
-
+I didn't finish evaluating bodies with stack_frames=1, so I used stack_frames=4.
+Be careful don't use this dataset to test if stack_frames is better than non-stack_Frames, because it was selected by sorting.
--- project/experiments/exp_007_body_dataset_3/src/sort_bodies_by_training_time.py
+++ project/experiments/exp_007_body_dataset_3/src/sort_bodies_by_training_time.py
@@ -1,4 +1,3 @@
-from genericpath import exists
 import os
 import shutil
 import pandas as pd
@@ -8,10 +7,13 @@ args = common.args
 threshold = 1500
 
 for body_base in args.train_bodies:
-    results = pd.DataFrame(columns=["body","value"])
+    results = pd.DataFrame(columns=["body", "value", "succeed"])
     for idx in range(50):
         body = body_base+idx
-        path = f"output_data/tensorboard/model-{body}-stack4-sd0/PPO_1"
+        if args.stack_frames>1:
+            path = f"output_data/tensorboard/model-{body}-stack4-sd0/PPO_1"
+        else:
+            path = f"output_data/tensorboard/model-{body}-sd0/PPO_1"
         if not os.path.exists(path):
             continue
         df = tflog2pandas(path)
@@ -34,11 +36,16 @@ for body_base in args.train_bodies:
     selected_bodies = results["body"].to_numpy().tolist()
     print(selected_bodies)
     
-    os.makedirs("output_data/selected_bodies", exist_ok=True)
     
+    if args.stack_frames>1:
+        folder = "output_data/selected_bodies_sf"
+    else:
+        folder = "output_data/selected_bodies"
+    os.makedirs(folder, exist_ok=True)
+
     re_index = {3:0,4:0,5:0,6:0}
     for body in selected_bodies:
         body = int(body)
         body_type = body//100
-        shutil.copy(f"output_data/bodies/{body}.xml", f"output_data/selected_bodies/{body_type*100+re_index[body_type]}.xml")
+        shutil.copy(f"output_data/bodies/{body}.xml", f"{folder}/{body_type*100+re_index[body_type]}.xml")
         re_index[body_type] += 1
