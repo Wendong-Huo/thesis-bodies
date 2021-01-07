d78a0ef2d24516e872af7f85b643874f998ce874
sida <sliu1@uvm.edu>
2021-01-07 16:21:36 -0500

    update

--- project/experiments/exp_011_preserve_header/src/start_experiment.sh
+++ project/experiments/exp_011_preserve_header/src/start_experiment.sh
@@ -19,11 +19,12 @@ do
     # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415 --test_bodies=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415 --random_align_obs --preserve_header, submitted
     # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515 --test_bodies=500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515 --random_align_obs --preserve_header
     # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615 --test_bodies=600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615 --random_align_obs --preserve_header
+    # submitted
+    # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315 --test_bodies=300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315 --random_align_obs --preserve_header --preserve_feet_contact
+    # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415 --test_bodies=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415 --random_align_obs --preserve_header --preserve_feet_contact
+    # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515 --test_bodies=500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515 --random_align_obs --preserve_header --preserve_feet_contact
+    # sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615 --test_bodies=600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615 --random_align_obs --preserve_header --preserve_feet_contact
 
-    sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315 --test_bodies=300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315 --random_align_obs --preserve_header --preserve_feet_contact
-    sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415 --test_bodies=400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415 --random_align_obs --preserve_header --preserve_feet_contact
-    sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515 --test_bodies=500,501,502,503,504,505,506,507,508,509,510,511,512,513,514,515 --random_align_obs --preserve_header --preserve_feet_contact
-    sbatch -J $EXP_FOLDER submit.sh python 1.train.py --seed=$seed --train_bodies=600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615 --test_bodies=600,601,602,603,604,605,606,607,608,609,610,611,612,613,614,615 --random_align_obs --preserve_header --preserve_feet_contact
 done
 
 
--- project/experiments/exp_011_preserve_header/src/submit.sh
+++ project/experiments/exp_011_preserve_header/src/submit.sh
@@ -2,6 +2,7 @@
 #SBATCH --partition bluemoon
 #SBATCH --mem 2G
 #SBATCH -c 1
+#SBATCH -B *:7
 #SBATCH --output /users/s/l/sliu1/slurm.out/%j-%x
 
 cd ${SLURM_SUBMIT_DIR}
