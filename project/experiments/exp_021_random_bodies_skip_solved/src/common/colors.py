f9f4f2237e6e20b7aee60c1ba0956ea3f0551271
sida <sliu1@uvm.edu>
2021-01-15 10:05:23 -0500

    self collision

--- project/experiments/exp_021_random_bodies_skip_solved/src/common/colors.py
+++ project/experiments/exp_021_random_bodies_skip_solved/src/common/colors.py
@@ -3,17 +3,17 @@ import numpy as np
 # color pallete from https://colorbrewer2.org/#type=qualitative&scheme=Paired&n=10
 link_rgba = np.array([
     # == first 8
-    [166, 206, 227, 255],
-    [31, 120, 180, 255],
-    [178, 223, 138, 255],
-    [51, 160, 44, 255],
-    [251, 154, 153, 255],
-    [227, 26, 28, 255],
-    [253, 191, 111, 255],
-    [255, 127, 0, 255],
+    [166, 206, 227, 255], # Light Blue
+    [31, 120, 180, 255],  # Dark Blue
+    [178, 223, 138, 255], # Light Green
+    [51, 160, 44, 255],   # Dark Green
+    [251, 154, 153, 255], # Pink
+    [227, 26, 28, 255],   # Red
+    [253, 191, 111, 255], # Yellow
+    [255, 127, 0, 255],   # Orange
     # == last 2
-    [202, 178, 214, 255],
-    [106, 61, 154, 255]
+    [202, 178, 214, 255], # Light Purple
+    [106, 61, 154, 255]   # Dark Purple
 ], dtype=np.float32)
 
 link_rgba = link_rgba / 255
--- project/experiments/exp_021_random_bodies_skip_solved/src/gym_envs/randomrobot.py
+++ project/experiments/exp_021_random_bodies_skip_solved/src/gym_envs/randomrobot.py
@@ -5,7 +5,7 @@ from gym_envs.my_envs import MyWalkerBase, MyWalkerBaseBulletEnv
 class MyRandomRobot(MyWalkerBase):
     foot_list = []
 
-    def __init__(self, xml):
+    def __init__(self, xml, self_collision=False):
         with open(xml, "r") as f:
             lines = f.readlines()
         joint_number = 0
@@ -15,6 +15,8 @@ class MyRandomRobot(MyWalkerBase):
         assert joint_number!=0
         print(f"joint_number: {joint_number}")
         super().__init__(xml, "torso", action_dim=joint_number, obs_dim=8+joint_number*2, power=0.10)
+        self.self_collision = self_collision
+        self.self_collision_enabled = False
 
     def alive_bonus(self, z, pitch):
         if z>self.initial_z*3: # avoid fly-away bug
@@ -29,20 +31,38 @@ class MyRandomRobot(MyWalkerBase):
         # bullet_client.setGravity(0,0,-1) # experiment in low gravity because of the pybullet "fly-away" bug
         bullet_client.changeVisualShape(1,-1,rgbaColor=[0.7,0.7,0.2,1.0]) # change torso color
         bullet_client.changeDynamics(0,-1,lateralFriction=0.999) # increase floor friction
-        # for j in self.ordered_joints:
-            # j
-        # robot_aabb = bullet_client.getAABB(1)
-        # height = robot_aabb[0][2]
-        # print(f"height:", height)
-        # height = -height + 1
-        # bullet_client.resetBasePositionAndOrientation(1,[0,0,height],[0,0,0,1])
+        if self.self_collision and not self.self_collision_enabled:
+            self.set_self_collision(bullet_client)
+            self.self_collision_enabled = True
 
+    def set_self_collision(self, bullet_client):
+        """need to do this manually: https://github.com/bulletphysics/bullet3/issues/1740"""
+        all_link_ids = [-1]
+        all_parent_relationship = {}
+        for j in self.ordered_joints:
+            ret = bullet_client.getJointInfo(1,j.jointIndex)
+            # print(ret)
+            # print(f"joint index: {ret[0]}, link name: {ret[-5]}, parent index: {ret[-1]})")
+            linkIndex = ret[0]+1 # TODO: Is this always the case? linkId = jointId+1
 
+            all_link_ids.append(linkIndex)
+            all_parent_relationship[linkIndex] = ret[-1]
+        
+        for j1 in all_link_ids:
+            for j2 in all_link_ids:
+                if j1<=j2: # only check each pair once
+                    continue
+                if all_parent_relationship[j1]==j2: # equivalent to URDF_USE_SELF_COLLISION_INCLUDE_PARENT
+                    continue
+                # print(f"set self collision pair: {j1},{j2}")
+                bullet_client.setCollisionFilterPair(1,1,j1,j2,1)
 
 class MyRandomRobotEnv(MyWalkerBaseBulletEnv):
 
     def __init__(self, xml, render=False):
-        self.robot = MyRandomRobot(xml=xml)
+        # Warning: Enable self collision will increase the possibility of the "Fly-away" bug!
+        #          The body plan will perform differently if this attribute is different, as if it is a completely new body plan. Need to re-search for workable bodies.
+        self.robot = MyRandomRobot(xml=xml, self_collision=True)
         self.xml = xml
         super().__init__(self.robot, render)
 
